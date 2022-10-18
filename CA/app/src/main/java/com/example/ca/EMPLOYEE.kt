92253fe028cf759dd31f9576fa575e70541bc089
Sanjeev choubey <Sanjeevchoubey215@gmail.com>
2022-10-18 17:52:12 +0530

    directory cleanup

--- CA/app/src/main/java/com/example/ca/EMPLOYEE.kt
+++
@@ -1,3 +0,0 @@
-package com.example.ca
-
-data class EMPLOYEE(val name: String, val age: String, val salary: String)
--- CodeforcesCompanion/app/src/main/java/com/example/codeforcescompanion/Fragment2.kt
+++
@@ -1,7 +0,0 @@
-package com.example.codeforcescompanion
-
-import androidx.fragment.app.Fragment
-
-class Fragment2 : Fragment(R.layout.fragment2) {
-
-}
